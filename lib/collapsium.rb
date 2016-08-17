1523399362b41eac776f9452f9964dda934cb65f
Jens Finkhaeuser <jens@finkhaeuser.de>
2016-08-17 10:27:45 +0200

    Add different modules to main require

--- lib/collapsium.rb
+++ lib/collapsium.rb
@@ -9,8 +9,13 @@
 
 require 'collapsium/version'
 
-require 'collapsium/recursive_merge'
+require 'collapsium/environment_override'
 require 'collapsium/indifferent_access'
 require 'collapsium/pathed_access'
+require 'collapsium/prototype_match'
+require 'collapsium/recursive_dup'
+require 'collapsium/recursive_merge'
+require 'collapsium/recursive_sort'
+require 'collapsium/viral_capabilities'
 
 require 'collapsium/uber_hash'
