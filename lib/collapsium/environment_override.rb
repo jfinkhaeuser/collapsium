08855a4bacd32cc38c271bd84f41a00531252a59
Jens Finkhaeuser <jens@finkhaeuser.de>
2016-08-18 12:44:01 +0200

    Fix functionality to follow new specs: - Consult more environment variables to honour all aspects of   PathedAccess - When fetching the old, non-overridden value, disable the environment   variable temporarily - otherwise we're looping. - Use RecursiveDup when creating temporary doubles

--- lib/collapsium/environment_override.rb
+++ lib/collapsium/environment_override.rb
@@ -10,6 +10,7 @@
 require 'collapsium/support/hash_methods'
 require 'collapsium/support/methods'
 
+require 'collapsium/recursive_dup'
 require 'collapsium/viral_capabilities'
 
 module Collapsium
@@ -27,8 +28,12 @@ module Collapsium
   # For example, the key "some!@email.org" will become "SOME_EMAIL_ORG".
   #
   # If PathedAccess is also used, the :path_prefix of nested hashes will
-  # be consulted after converting it in the same manner.
+  # be consulted after converting it in the same manner. NOTE:
+  # include EnvironmentOverride *after* PathedAccess for both to work well
+  # together.
   module EnvironmentOverride
+    include ::Collapsium::RecursiveDup
+
     ##
     # If the module is included, extended or prepended in a class, it'll
     # wrap accessor methods.
@@ -53,18 +58,39 @@ module Collapsium
         # All KEYED_READ_METHODS have a key as the first argument.
         key = args[0]
 
-        # Grab matching environment variable names. We consider first a pathed
-        # name, if pathed access is supported, followed by the unpathed name.
-        env_keys = [key.to_s]
-        if receiver.respond_to?(:path_prefix) and not
-            (receiver.path_prefix.nil? or receiver.path_prefix.empty?)
-          prefix_components = receiver.path_components(receiver.path_prefix)
+        # Grab matching environment variable names. If PathedAccess is
+        # supported, we'll try environment variables of the path, starting
+        # with the full qualified path and ending with just the last key
+        # component.
+        env_keys = []
+        if receiver.respond_to?(:path_prefix)
 
-          if prefix_components.last == key
-            env_keys.unshift(receiver.path_prefix)
-          else
-            env_keys.unshift(receiver.path_prefix + receiver.separator + key.to_s)
+          # If we have a prefix, use it.
+          components = []
+          if not receiver.path_prefix.nil?
+            components += receiver.path_components(receiver.path_prefix)
           end
+
+          # The key has its own components. If the key components and prefix
+          # components overlap (it happens), ignore the duplication.
+          key_comps = receiver.path_components(key.to_s)
+          if key_comps.first == components.last
+            components.pop
+          end
+          components += key_comps
+
+          # Start with most qualified, shifting off the first component
+          # until we reach just the last component.
+          loop do
+            path = receiver.normalize_path(components)
+            env_keys << path
+            components.shift
+            if components.empty?
+              break
+            end
+          end
+        else
+          env_keys = [key.to_s]
         end
         env_keys.map! { |k| receiver.key_to_env(k) }
         env_keys.select! { |k| not k.empty? }
@@ -84,29 +110,37 @@ module Collapsium
           # the parsed result. Otherwise use it as a string.
           require 'json'
           # rubocop:disable Lint/HandleExceptions
+          parsed = env_value
           begin
-            env_value = JSON.parse(env_value)
+            parsed = JSON.parse(env_value)
           rescue JSON::ParserError
             # Do nothing. We just use the env_value verbatim.
           end
           # rubocop:enable Lint/HandleExceptions
 
-          # Note that we re-assign the value only if it's changed, but we
-          # break either way. If env_value exists, it must be used, but
-          # changing it always will lead to an infinite recursion.
-          # The double's wrapped_method will never be called, but rather
-          # always this wrapper.
-          if env_value != receiver[key]
-            value = env_value
+          # Excellent, we've got an environment variable. Only use it if it
+          # changes something, though. We'll temporarily unset the environment
+          # variable while fetching the old value.
+          ENV.delete(env_key)
+          old_value = receiver[key]
+          ENV[env_key] = env_value # not the parsed value!
+
+          if parsed != old_value
+            value = parsed
           end
           break
         end
+
         if not value.nil?
           # We can't just return the value, because that doesn't respect the
-          # method being called. We can deal with this by duplicating the
-          # receiver, writing the value we found into the appropriate key,
-          # and then sending the wrapped_method to the duplicate.
-          double = receiver.dup
+          # method being called. We also can't store the value, because that
+          # would not reset the Hash after the environment variable was
+          # cleared.
+          #
+          # We can deal with this by duplicating the receiver, writing the value
+          # we found into the appropriate key, and then sending the
+          # wrapped_method to the duplicate.
+          double = receiver.recursive_dup
           double[key] = value
           meth = double.method(wrapped_method.name)
           next meth.call(*args, &block)
@@ -129,7 +163,7 @@ module Collapsium
       end
 
       def enhance(base)
-        # Make the capabilities of classes using PathedAccess viral.
+        # Make the capabilities of classes using EnvironmentOverride viral.
         base.extend(ViralCapabilities)
 
         # Wrap read accessor functions to deal with paths
