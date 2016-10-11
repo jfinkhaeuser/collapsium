a6a4bde5601fc4d0eca0be1da3a7b2e87061b927
Jens Finkhaeuser <jens@finkhaeuser.de>
2016-10-11 15:35:04 +0200

    Use PathComponents in PathedAccess in favour of own implementation.

--- lib/collapsium/pathed_access.rb
+++ lib/collapsium/pathed_access.rb
@@ -9,6 +9,7 @@
 
 require 'collapsium/support/hash_methods'
 require 'collapsium/support/methods'
+require 'collapsium/support/path_components'
 
 require 'collapsium/viral_capabilities'
 
@@ -27,32 +28,7 @@ module Collapsium
   # for a path.
   module PathedAccess
 
-    # @return [String] the separator is the character or pattern splitting paths.
-    def separator
-      @separator ||= DEFAULT_SEPARATOR
-      return @separator
-    end
-
-    ##
-    # Assume any pathed access has this prefix.
-    def path_prefix=(value)
-      @path_prefix = normalize_path(value)
-    end
-
-    def path_prefix
-      @path_prefix ||= ''
-      return @path_prefix
-    end
-
-    # @api private
-    # Default path separator
-    DEFAULT_SEPARATOR = '.'.freeze
-
-    ##
-    # @return [RegExp] the pattern to split paths at; based on `separator`
-    def split_pattern
-      /(?<!\\)#{Regexp.escape(separator)}/
-    end
+    include ::Collapsium::Support::PathComponents
 
     ##
     # If the module is included, extended or prepended in a class, it'll
@@ -187,39 +163,6 @@ module Collapsium
         return recursive_fetch(tail, data[head], current_path, options)
       end
     end # class << self
-
-    ##
-    # Break path into components. Expects a String path separated by the
-    # `#separator`, and returns the path split into components (an Array of
-    # String).
-    def path_components(path)
-      return filter_components(path.split(split_pattern))
-    end
-
-    ##
-    # Given path components, filters out unnecessary ones.
-    def filter_components(components)
-      return components.select { |c| not c.nil? and not c.empty? }
-    end
-
-    ##
-    # Join path components with the `#separator`.
-    def join_path(components)
-      return components.join(separator)
-    end
-
-    ##
-    # Normalizes a String path so that there are no empty components, and it
-    # starts with a separator.
-    def normalize_path(path)
-      components = []
-      if path.respond_to?(:split) # likely a String
-        components = path_components(path)
-      elsif path.respond_to?(:join) # likely an Array
-        components = filter_components(path)
-      end
-      return separator + join_path(components)
-    end
 
     ##
     # Ensure that all values have their path_prefix set.
