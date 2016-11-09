411ddcd9c49c5cd39085a468be1ad206015d9384
Jens Finkhaeuser <jens@finkhaeuser.de>
2016-11-09 17:34:19 +0100

    Reimplement IndifferentAccess as an includable module rather than based on the faulty default_proc approach. Bonus: also integer conversions (for Arrays) included.

--- lib/collapsium/indifferent_access.rb
+++ lib/collapsium/indifferent_access.rb
@@ -7,6 +7,11 @@
 # All rights reserved.
 #
 
+require 'collapsium/support/hash_methods'
+require 'collapsium/support/methods'
+
+require 'collapsium/viral_capabilities'
+
 module Collapsium
 
   ##
@@ -16,18 +21,75 @@ module Collapsium
   module IndifferentAccess
 
     ##
-    # Set your Hash's #default_proc to DEFAULT_PROC, and you've got indifferent
-    # access.
-    DEFAULT_PROC = proc do |hash, key|
-      case key
-      when String
-        sym = key.to_sym
-        hash[sym] if hash.key?(sym)
-      when Symbol
-        str = key.to_s
-        hash[str] if hash.key?(str)
+    # If the module is included, extended or prepended in a class, it'll
+    # wrap accessor methods.
+    class << self
+      include ::Collapsium::Support::Methods
+
+      READ_METHODS = (
+        ::Collapsium::Support::HashMethods::KEYED_READ_METHODS \
+        + ::Collapsium::Support::ArrayMethods::INDEXED_READ_METHODS
+      ).uniq.freeze
+
+      INDIFFERENT_ACCESS_READER = proc do |wrapped_method, key, *args, &block|
+        # Definitely try the key as given first. Then, depending on the key's
+        # type and value, we want to try it as a Symbol, String and/or Integer
+        tries = [key]
+        if key.is_a? Symbol
+          key_s = key.to_s
+          tries << key_s
+          if key_s =~ /^[0-9]/
+            tries << key_s.to_i
+          end
+        elsif key.is_a? String
+          tries << key.to_sym
+          if key =~ /^[0-9]/
+            tries << key.to_i
+          end
+        elsif key.is_a? Integer
+          tries += [key.to_s, key.to_s.to_sym]
+        end
+
+        # With the variations to try assembled, go through them one by one
+        result = nil
+        receiver = wrapped_method.receiver
+        tries.each do |try|
+          if receiver.keys.include?(try)
+            result = wrapped_method.call(try, *args, &block)
+            break
+          end
+        end
+
+        # If any of the above yielded a result, great, return that. Otherwise
+        # yield to the default implementation (i.e. wrapped_method).
+        if not result.nil?
+          next result
+        end
+        next wrapped_method.call(key, *args, &block)
+      end.freeze
+
+      def included(base)
+        enhance(base)
       end
-    end.freeze
+
+      def extended(base)
+        enhance(base)
+      end
+
+      def prepended(base)
+        enhance(base)
+      end
+
+      def enhance(base)
+        # Make the capabilities of classes using PathedAccess viral.
+        base.extend(ViralCapabilities)
+
+        # Wrap all accessor functions to deal with paths
+        READ_METHODS.each do |method|
+          wrap_method(base, method, raise_on_missing: false, &INDIFFERENT_ACCESS_READER)
+        end
+      end
+    end # class << self
 
   end # module IndifferentAccess
 
--- spec/indifferent_access_spec.rb
+++ spec/indifferent_access_spec.rb
@@ -1,10 +1,23 @@
 require 'spec_helper'
 require_relative '../lib/collapsium/indifferent_access'
 
+class IncludedIndifferentHash < Hash
+  include ::Collapsium::IndifferentAccess
+end
+
+class PrependedIndifferentHash < Hash
+  prepend ::Collapsium::IndifferentAccess
+end
+
+class ExtendedIndifferentHash < Hash
+  extend ::Collapsium::IndifferentAccess
+end
+
+
 describe ::Collapsium::IndifferentAccess do
   let(:tester) do
     tester = {}
-    tester.default_proc = ::Collapsium::IndifferentAccess::DEFAULT_PROC
+    tester.extend(::Collapsium::IndifferentAccess)
     tester
   end
 
@@ -24,10 +37,55 @@ describe ::Collapsium::IndifferentAccess do
     expect(tester["foo"]).to eql 42
   end
 
+  it "allows accessing integer keys with strings or symbols" do
+    expect(tester).to be_empty
+
+    tester[42] = 123
+    expect(tester[:"42"]).to eql 123
+    expect(tester["42"]).to eql 123
+  end
+
+  it "allows accessing string keys with integers" do
+    expect(tester).to be_empty
+
+    tester["42"] = 123
+    expect(tester[:"42"]).to eql 123
+    expect(tester[42]).to eql 123
+  end
+
   it "still works with other keys" do
     expect(tester).to be_empty
 
     tester[42] = "foo"
-    expect(tester[42]).to eql "foo"
+
+    expect(tester[nil]).to be_nil
+    expect(tester[3.14]).to be_nil
+  end
+
+  context IncludedIndifferentHash do
+    let(:tester) { IncludedIndifferentHash.new }
+
+    it "can be accessed indifferently" do
+      tester["foo"] = 42
+      expect(tester[:foo]).to eql 42
+    end
+  end
+
+  context ExtendedIndifferentHash do
+    let(:tester) { ExtendedIndifferentHash.new }
+
+    it "can be accessed indifferently" do
+      tester["foo"] = 42
+      expect(tester[:foo]).to eql 42
+    end
+  end
+
+  context PrependedIndifferentHash do
+    let(:tester) { PrependedIndifferentHash.new }
+
+    it "can be accessed indifferently" do
+      tester["foo"] = 42
+      expect(tester[:foo]).to eql 42
+    end
   end
 end
