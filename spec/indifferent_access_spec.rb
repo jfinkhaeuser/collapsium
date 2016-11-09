70bb80368273b9ad9190f579772151a92a95f8d7
Jens Finkhaeuser <jens@finkhaeuser.de>
2016-11-09 14:11:32 +0100

    Refactor IndifferentAccess specs a little

--- spec/indifferent_access_spec.rb
+++ spec/indifferent_access_spec.rb
@@ -2,25 +2,32 @@ require 'spec_helper'
 require_relative '../lib/collapsium/indifferent_access'
 
 describe ::Collapsium::IndifferentAccess do
-  before :each do
-    @tester = {}
-    @tester.default_proc = ::Collapsium::IndifferentAccess::DEFAULT_PROC
+  let(:tester) do
+    tester = {}
+    tester.default_proc = ::Collapsium::IndifferentAccess::DEFAULT_PROC
+    tester
   end
 
   it "allows accessing string keys via symbol" do
-    @tester["foo"] = 42
-    expect(@tester["foo"]).to eql 42
-    expect(@tester[:foo]).to eql 42
+    expect(tester).to be_empty
+
+    tester["foo"] = 42
+    expect(tester["foo"]).to eql 42
+    expect(tester[:foo]).to eql 42
   end
 
   it "allows accessing symbol keys via strings" do
-    @tester[:foo] = 42
-    expect(@tester[:foo]).to eql 42
-    expect(@tester["foo"]).to eql 42
+    expect(tester).to be_empty
+
+    tester[:foo] = 42
+    expect(tester[:foo]).to eql 42
+    expect(tester["foo"]).to eql 42
   end
 
   it "still works with other keys" do
-    @tester[42] = "foo"
-    expect(@tester[42]).to eql "foo"
+    expect(tester).to be_empty
+
+    tester[42] = "foo"
+    expect(tester[42]).to eql "foo"
   end
 end
