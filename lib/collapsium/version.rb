75b14b292d96cff8eaa1314f9b86ebc00b668590
Jens Finkhaeuser <jens@finkhaeuser.de>
2018-04-09 11:22:08 +0200

    Bump own version

--- lib/collapsium/version.rb
+++ lib/collapsium/version.rb
@@ -3,10 +3,10 @@
 # collapsium
 # https://github.com/jfinkhaeuser/collapsium
 #
-# Copyright (c) 2016-2017 Jens Finkhaeuser and other collapsium contributors.
+# Copyright (c) 2016-2018 Jens Finkhaeuser and other collapsium contributors.
 # All rights reserved.
 #
 module Collapsium
   # The current release version
-  VERSION = "0.9.1".freeze
+  VERSION = "0.10.0".freeze
 end
